1b63ab9e297d07a83ba1d5d9d2a054a4d6555fd2
Krish Soni <isonikrish@gmail.com>
2024-11-24 19:32:13 +0530

    Auth/login

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,15 +1,18 @@
 import { generateToken } from '../lib/utils.js';
 import User from '../models/user.js';
 import bcrypt from 'bcryptjs'
-export async function handleSignup(req,res){
-    const {fullName , email, password} = req.body;
+export async function handleSignup(req, res) {
+    const { fullName, email, password } = req.body;
     try {
-        if(password.length < 6){
-            return res.status(400).json({msg: "Password must be atleast 6 characters"});
+        if (!fullName || !email || !password) {
+            return res.status(400).json({ msg: "All fields are required" })
         }
-        const user = await User.findOne({email});
-        if(user){
-            return res.status(400).json({msg: "User already exists"});
+        if (password.length < 6) {
+            return res.status(400).json({ msg: "Password must be atleast 6 characters" });
+        }
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ msg: "User already exists" });
         }
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
@@ -18,21 +21,34 @@ export async function handleSignup(req,res){
             email,
             password: hashedPassword,
         })
-        if(newUser){
+        if (newUser) {
             //generate Token here
-            generateToken(newUser._id,res);
+            generateToken(newUser._id, res);
             await newUser.save();
             res.status(201).json(newUser);
-        }else{
-            res.status(400).json({msg: "Invalid User Data"});
+        } else {
+            res.status(400).json({ msg: "Invalid User Data" });
         }
     } catch (error) {
-        res.status(500).json({msg: "Internal server error"});
+        res.status(500).json({ msg: "Internal server error" });
     }
 }
-export async function handleLogin(req,res){
+export async function handleLogin(req, res) {
+    const { email, password } = req.body;
+    try {
+        if (!email || !password) return res.status(400).json({ msg: "All fields are required" })
+        const user = await User.findOne({ email });
+        if (!user) return res.status(400).json({ msg: "Invalid credentials" });
+        const isPasswordCorrect = await bcrypt.compare(password,user.password);
+        if (!isPasswordCorrect) return res.status(400).json({ msg: "Invalid password" });
 
+        generateToken(user._id,res);
+        res.status(200).json({msg: "Logged In"})
+
+    } catch (error) {
+        res.status(500).json({ msg: "Internal server error" });
+    }
 }
-export async function handleLogout(req,res){
+export async function handleLogout(req, res) {
 
 }
